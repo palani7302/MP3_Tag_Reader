db6409320afbbc6a43a19fb6f183f31f75be5afe
Palani <palanimohan06@gmail.com>
2024-03-28 21:06:43 +0530

    Created MP3 Tag Reader Project

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> //including library
+/*
+Name			: Palani M
+Date			:
+Title			:
+Sample Input	:
+Sample output	:
+*/
+int main()
+{
+	printf("MP3 Tag Reader\n");
+
+    return 0;
+}
+
